61c899a90e5d65a5739e4775a0d107823c3c0b6a
Furqaan Nabi <0xfurqaan@gmail.com>
2026-05-17 19:16:41 +0530

    feat: add Sui configuration, enclave matching logic, and setup project metadata

--- web/src/lib/sui.ts
+++ web/src/lib/sui.ts
@@ -30,7 +30,7 @@ const TESTNET: NetworkConfig = {
   shellPackageId: '0x5a47e78620e79a131bb8115a8f9e41f0bba0e387ec4c0ed93514853bd9987fbd',
   poolId: '0xedc28f54b442ab2422ed8bab35e7a4ebcbc96baa7393d61704fc633503fccdae',
   enclaveConfigId: '0x741c7a6cf78930ca2dea0d3188749be18585d286e5c28bfdef007aff3468f41f',
-  enclaveId: '0x1b18a55393efa9378c11e4eac0ad94c3ec3759f85be6c92f71a7a3b074b871e1',
+  enclaveId: '0xf7831c3d41c77a1ff170466bcf5b57b3d602b152ed0d756c49b47bb4c00517c1',
   enclaveUrl: 'http://54.80.82.200:3000',
   deepbookPoolKey: 'SUI_DBUSDC',
   quoteCoinType: '0xe95040085976bfd54a1a07225cd46c8a2b4e8e2b6732f140a0fc49850ba73e1a::dusdc::DUSDC',
